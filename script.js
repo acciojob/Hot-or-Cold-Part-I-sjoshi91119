b0edc02b89a6268d1648fc5b353686a7692e2f51
Shubham Joshi <115713904+sjoshi91119@users.noreply.github.com>
2023-02-27 13:55:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,43 +1,21 @@
 
 //your code here
-// Generate a random number between 1 and 100
-const secretNumber = Math.floor(Math.random() * 100) + 1;
-
-// Store the previous guess
-let previousGuess;
-
-function checkGuess() {
-  // Get the user's guess from the input field
-  const guess = parseInt(document.getElementById("guessInput").value);
-
-  if (isNaN(guess) || guess < 1 || guess > 100) {
-    // If the user's guess is not a valid number, show an error message
-    document.getElementById("response").textContent = "Please enter a number between 1 and 100.";
-  } else if (guess === secretNumber) {
-    // If the user's guess is correct, show a success message
-    document.getElementById("response").textContent = "Congratulations, you guessed the number!";
-  } else {
-    // Calculate the difference between the user's guess and the secret number
-    const difference = Math.abs(guess - secretNumber);
-
-    if (previousGuess === undefined) {
-      // If this is the first guess, show a hint to go higher or lower
-      document.getElementById("response").textContent = guess > secretNumber ? "Guess lower" : "Guess higher";
-    } else {
-      // Calculate the difference between the previous guess and the secret number
-      const previousDifference = Math.abs(previousGuess - secretNumber);
-
-      if (difference < previousDifference) {
-        // If the user is getting closer to the secret number, show a "getting hotter" message
-        document.getElementById("response").textContent = guess > previousGuess ? "Getting hotter, guess lower" : "Getting hotter, guess higher";
-      } else {
-        // If the user is getting further from the secret number, show a "getting colder" message
-        document.getElementById("response").textContent = guess > previousGuess ? "Getting colder, guess higher" : "Getting colder, guess lower";
-      }
-    }
-
-    // Store the current guess as the previous guess for the next round
-    previousGuess = guess;
-  }
+var randomNum;
+var input;
+function randomNumGenerator(){
+	randomNum = Math.floor(Math.random()*41) - 20;
+	document.getElementById("num").innerHTML = randomNum;
+	guessName();
+	print();
 }
-
+function guessName() {
+	input = parseInt(document.getElementById("guess").value);	
+}
+function print(){
+	if(Math.abs(randomNum - input) <= 5){
+		document.getElementById("respond").innerHTML = "Hot";
+	}
+	else{
+		document.getElementById("respond").innerHTML = "Cold";
+	}
+}
